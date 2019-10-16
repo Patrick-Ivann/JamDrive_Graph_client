aa7d0fc059bdb241e2a8326d17fb2f4e5fc73e84
Patrick-Ivann <33460507+Patrick-Ivann@users.noreply.github.com>
2019-10-16 15:53:34 +0200

    :hammer: Route update

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import { Router, Route, Switch } from "react-router-dom";
 
 
 import './App.css';
-import Header from './component/layout/Header';
 // import PrivateRoute from './component/common/PrivateRoute';
 import { graphql } from 'react-apollo';
 import { CURRENT_USER_QUERY } from './graphql/queries/Auth';
 import LoginIndex from './component/Login/LoginIndex';
-import PrositFeedIndex from './component/PrositFeed/PrositFeedIndex';
 import Page from './component/common/ErrorPage';
 import history from './utils/history';
 
 import jwt_decode from "jwt-decode";
+import PrositfFeedBloc from './component/PrositFeed/PrositfFeedBloc';
+import HeaderBloc from './component/layout/HeaderBloc';
 
 
 
@@ -26,12 +26,12 @@ function App({ client,
   called })   {
 
 useEffect(() => {
-  console.log(client)
-  console.log(loading)
-  console.log(error)
-  console.log(networkStatus)
-  console.log(called)
-  console.log(data)
+  // console.log(client)
+  // console.log(loading)
+  // console.log(error)
+  // console.log(networkStatus)
+  // console.log(called)
+  // console.log(data)
   if (localStorage[process.env.REACT_APP_LOCAL_TOKEN]) {   
     const decoded = jwt_decode(localStorage[process.env.REACT_APP_LOCAL_TOKEN])
     const currentTime = Date.now() / 1000;
@@ -54,11 +54,11 @@ if (error) return <div > error </div>
 return (
 
       <Router history={history}>
-        <Header/>
+        <HeaderBloc/>
           <Route exact path="/" component={LoginIndex} />
           {/* <PrivateRoute currentUser={user} exact path='/prosits' component={PrositFeedIndex} /> */}
         <Switch>
-          <Route exact path="/feed" component={PrositFeedIndex} />
+          <Route exact path="/feed" component={PrositfFeedBloc} />
 
           {/* <PrivateRoute currentUser={user} exact path='/prositsAdmin'  component={PrositFeedIndex}/> */}
         </Switch>
