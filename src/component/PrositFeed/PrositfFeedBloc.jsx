3bca12a035568f86ce5368894d9e9b562f10339f
Patrick-Ivann <33460507+Patrick-Ivann@users.noreply.github.com>
2019-10-20 22:59:00 +0200

    :hammer: Fixing Local Cache related issues on various component.

--- src/component/PrositFeed/PrositfFeedBloc.jsx
+++ src/component/PrositFeed/PrositfFeedBloc.jsx
@@ -1,35 +1,33 @@
-import React, { useEffect } from 'react'
-import { PROSIT_FEED_BY_CLASS } from '../../graphql/queries/Prosit';
-import PrositFeedIndex from './PrositFeedIndex';
-import { useQuery } from 'react-apollo-hooks';
-import { LOCAL_FILTERSTORE_QUERY } from '../../graphql/local/localQueries';
+import React, { useEffect } from "react";
+import { PROSIT_FEED_BY_CLASS } from "../../graphql/queries/Prosit";
+import PrositFeedIndex from "./PrositFeedIndex";
+import { useQuery } from "react-apollo-hooks";
+import { LOCAL_FILTERSTORE_QUERY } from "../../graphql/local/localQueries";
 
 export default function PrositfFeedBloc() {
+  const { client, data, error, loading, networkStatus, called } = useQuery(
+    PROSIT_FEED_BY_CLASS,
+    { fetchPolicy: "network-only" }
+  );
 
-    const { client,
-        data,
-        error,
-        loading,
-        networkStatus,
-        called } = useQuery(PROSIT_FEED_BY_CLASS, { fetchPolicy: "network-only" });
+  const {
+    client: localClient,
+    data: filterStore,
+    error: localError,
+    loading: localLoading,
+    networkStatus: localNetworkStatus,
+    localClient: localCalled
+  } = useQuery(LOCAL_FILTERSTORE_QUERY);
+  if (localError) console.error(localError);
 
-
-    const { client: localClient,
-        data: filterStore,
-        error: localError,
-        loading: localLoading,
-        networkStatus: localNetworkStatus,
-        localClient: localCalled } = useQuery(LOCAL_FILTERSTORE_QUERY)
-    if (localLoading) return <div>fqsdfsdfsd</div>
-
-
-
-
-
-
-    return (
-        <>
-            <PrositFeedIndex data={data} error={error} loading={loading} filterStore={filterStore ? filterStore : ""} ></PrositFeedIndex>
-        </>
-    )
+  return (
+    <>
+      <PrositFeedIndex
+        data={data}
+        error={error}
+        loading={loading}
+        filterStore={filterStore ? filterStore : ""}
+      ></PrositFeedIndex>
+    </>
+  );
 }
--- src/component/layout/HeaderBloc.jsx
+++ src/component/layout/HeaderBloc.jsx
@@ -1,27 +1,35 @@
-import React from 'react'
-import { LOCAL_THEMESTORE_QUERY, LOCAL_USERSTORE_QUERY } from '../../graphql/local/localQueries';
-import { useQuery } from 'react-apollo-hooks';
-import Header from './Header';
+import React from "react";
+import {
+  LOCAL_THEMESTORE_QUERY,
+  LOCAL_USERSTORE_QUERY
+} from "../../graphql/local/localQueries";
+import { useQuery } from "react-apollo-hooks";
+import Header from "./Header";
 
 export default function HeaderBloc() {
+  const {
+    client: localClient,
+    data: themeStore,
+    error: localError,
+    loading: localLoading,
+    networkStatus: localNetworkStatus,
+    localClient: localCalled
+  } = useQuery(LOCAL_THEMESTORE_QUERY);
 
-    const { client: localClient,
-        data: themeStore,
-        error: localError,
-        loading: localLoading,
-        networkStatus: localNetworkStatus,
-        localClient: localCalled } = useQuery(LOCAL_THEMESTORE_QUERY);
+  const {
+    client: localUserClient,
+    data: userStore,
+    error: localUserError,
+    loading: localUserLoading,
+    networkStatus: localUserNetworkStatus,
+    localClient: localUserCalled
+  } = useQuery(LOCAL_USERSTORE_QUERY);
 
-    const { client: localUserClient,
-        data: userStore,
-        error: localUserError,
-        loading: localUserLoading,
-        networkStatus: localUserNetworkStatus,
-        localClient: localUserCalled } = useQuery(LOCAL_USERSTORE_QUERY);
-
-    return (
-        <div>
-            {userStore && <Header themeStore={themeStore} ></Header>}
-        </div>
-    )
+  return (
+    <div>
+      {userStore.userStore.userData && (
+        <Header themeStore={themeStore}></Header>
+      )}
+    </div>
+  );
 }
