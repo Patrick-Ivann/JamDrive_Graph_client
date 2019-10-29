485a1ea040ace7c89b37bf0025b0daf8f0c55b73
Patrick-Ivann <33460507+Patrick-Ivann@users.noreply.github.com>
2019-10-29 20:13:29 +0100

    :paintbrush: Adding Footer new Component

---
+++ src/component/layout/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <>
+      <hr class="border-b m-0" />
+      <footer class="p-4 text-center">
+        <h3>Footer</h3>
+      </footer>
+    </>
+  );
+}
