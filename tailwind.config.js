9916daa64cfbcffab066e831e01a51e631a6546d
Patrick-Ivann <33460507+Patrick-Ivann@users.noreply.github.com>
2019-10-29 19:48:31 +0100

    :hammer: Tailwind configuration update to support filter plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,26 @@
 module.exports = {
   theme: {
-    extend: {}
+    filter: { // defaults to {}
+      'none': 'none',
+      'grayscale': 'grayscale(1)',
+      'invert': 'invert(1)',
+      'sepia': 'sepia(1)',
+    },
+    backdropFilter: { // defaults to {}
+      'none': 'none',
+      'blur': 'blur(20px)',
+    },
+    inset: {
+      '0': 0,
+     auto: 'auto',
+     '1/2': '50%',
+    }
   },
-  variants: {},
-  plugins: []
+  variants: {
+    filter: ['responsive,hover'], // defaults to ['responsive']
+    backdropFilter: ['responsive'], // defaults to ['responsive']
+  },
+  plugins: [
+    require('tailwindcss-filters')(),
+  ],
 }
