2c5de0c3bab33ee0e017393158762df20f1ede27
Patrick-Ivann <33460507+Patrick-Ivann@users.noreply.github.com>
2019-11-05 21:55:37 +0100

    :hammer:  refactoring

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { useEffect } from "react";
 import { Router, Route, Switch } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
 
-// import PrivateRoute from './component/common/PrivateRoute';
-import { graphql, useQuery, Query } from "react-apollo";
-import { CURRENT_USER_QUERY } from "./graphql/queries/Auth";
 import LoginIndex from "./component/Login/LoginIndex";
 import Page from "./component/common/ErrorPage";
 import history from "./utils/history";
@@ -15,26 +11,20 @@ import HeaderBloc from "./component/layout/HeaderBloc";
 import { useLogout } from "./graphql/handlers/authHandling";
 
 import { Container, MainBody } from "./styles/layout";
-import { LOCAL_THEMESTORE_QUERY } from "./graphql/local/localQueries";
 import ThemeProviderWrapper from "./styles/ThemeProviderWrapper";
 import Footer from "./component/layout/Footer";
-import Modal from "./component/common/Modal";
-import { useModal } from "./utils/customHooks";
-import Upload from "./component/PrositForm/Upload";
-import ModalMenu from "./component/common/ModalMenu";
 
-// function App({ client,
-//   data,
-//   error,
-//   loading,
-//   networkStatus,
-//   called })   {
+import { PrivateRoute } from "./component/common/PrivateRoute";
 
-function App(props) {
-  const clearUser = useLogout();
+function App(props)   {
 
-  useEffect(() => {
-    if (localStorage[process.env.REACT_APP_LOCAL_TOKEN]) {
+ 
+  // function App(props) {
+    const clearUser = useLogout();
+    
+    useEffect(() => {
+      
+      if (localStorage[process.env.REACT_APP_LOCAL_TOKEN]) {
       const decoded = jwt_decode(
         localStorage[process.env.REACT_APP_LOCAL_TOKEN]
       );
@@ -59,15 +49,15 @@ function App(props) {
             <HeaderBloc />
 
             <Route exact path="/" component={LoginIndex} />
-            {/* <PrivateRoute currentUser={user} exact path='/prosits' component={PrositFeedIndex} /> */}
+            <PrivateRoute exact path='/feed' component={PrositfFeedBloc} />
+            
             <Switch>
-              <Route exact path="/feed" component={PrositfFeedBloc} />
+              {/* <Route exact path="/feed" component={PrositfFeedBloc} /> */}
 
               {/* <PrivateRoute currentUser={user} exact path='/prositsAdmin'  component={PrositFeedIndex}/> */}
             </Switch>
-            <Route exact path="/error-page/:error" component={Page} />
-            <Route exact path="/drop" component={Upload}></Route>
-            <Route exact path="/pop" component={ModalMenu}></Route>
+            <Route exact path="/error-page/:error" Component={Page} />
+
           </Container>
         </ThemeProviderWrapper>
       </Router>
@@ -76,7 +66,7 @@ function App(props) {
   );
 }
 
-// export default graphql(CURRENT_USER_QUERY, {
+// export default graphql(LOCAL_USERSTORE_QUERY, {
 //   options: { fetchPolicy: 'network-only' }
 // })(App);
 
